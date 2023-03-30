0ca7b6170e0f705f86dc192a99edaaaff3c963e6
Patryk Kurzeja <patryk.kurzeja@infermedica.com>
2023-03-30 16:27:26 +0200

    Initial commit

---
+++ components/Profile.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+
+function Profile () {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  const styles = {
+    backgroundColor: '#eaeaff',
+    padding: '1.5rem',
+    borderRadius: '0.5rem',
+  }
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.bio}</p>
+    </div>
+  )
+}
+
+export default Profile
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(req, res) {
+res.status(200).json(
+  { name: 'John Doe',
+   bio: 'I am a software engineer, I love to code and I am a big fan of JavaScript'
+  })
 }
